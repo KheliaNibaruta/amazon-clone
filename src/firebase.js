b77cd0c369e7bcc9619d92e9e026131564a51eae
KheliaNibaruta <khelia.nibaruta@lacatholille.fr>
2024-01-08 13:39:25 +0100

    getting set up

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDsYEhPmS5n7HTdMXoEK2yiJOvDU8CbLPk",
+    authDomain: "clone-218ce.firebaseapp.com",
+    projectId: "clone-218ce",
+    storageBucket: "clone-218ce.appspot.com",
+    messagingSenderId: "907187243192",
+    appId: "1:907187243192:web:98b8338704fa8bcdae8c4d",
+    measurementId: "G-GMXDPEX6SH"
+  };
